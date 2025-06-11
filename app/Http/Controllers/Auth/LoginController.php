7e1ac01b637abc82e4f1edfc99667910f0e94286
emmanuelcc3 <75090995+emmanuelcc3@users.noreply.github.com>
2025-06-11 16:12:09 -0600

    Add LoginController for authentication

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,15 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+
+    public function logout(\Illuminate\Http\Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login'); 
+    }
+
 }
