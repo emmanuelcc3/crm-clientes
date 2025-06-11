6c86ed3f554560b1f7ee07f93c2ba970220a011e
emmanuelcc3 <75090995+emmanuelcc3@users.noreply.github.com>
2025-06-11 16:11:35 -0600

    Add initial models: Cliente, Etiqueta

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -4,11 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cliente extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    protected $fillable = ['nombre', 'correo', 'telefono', 'direccion', 'etiquetas'];
+    protected $fillable = ['nombre', 'correo', 'telefono'];
+
+
+public function etiquetas()
+    {
+        return $this->belongsToMany(\App\Models\Etiqueta::class, 'cliente_etiqueta', 'cliente_id', 'etiqueta_id');
+    }
+
 }
 
