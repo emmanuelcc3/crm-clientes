ab66b6b9f3e2245d68c883b8fafa1dd6c3498c66
emmanuelcc3 <75090995+emmanuelcc3@users.noreply.github.com>
2025-06-11 16:14:41 -0600

    Configure permission setup and update service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,6 +9,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
+    
+    
+
+
     public function register(): void
     {
         //
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,41 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\TrustProxies;
+use Illuminate\Http\Middleware\HandleCors;
+use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
+use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
+
+// Middleware de Spatie (nombre correcto: Middleware en singular)
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
+use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Middleware globales (siempre activos)
+        $middleware->append([
+            HandleCors::class,
+            TrustProxies::class,
+            ValidatePostSize::class,
+            ConvertEmptyStringsToNull::class,
+        ]);
+
+        // Aliases que puedes usar en rutas o controladores
+        $middleware->alias([
+            'auth' => Authenticate::class,
+            'permission' => PermissionMiddleware::class,
+            'role' => RoleMiddleware::class,
+            'role_or_permission' => RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
